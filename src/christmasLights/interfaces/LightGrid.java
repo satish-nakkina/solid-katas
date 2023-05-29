96815345ba49f9fc621fc5150b6df1ccc976c575
satish nakkina <satish.nakkina@zemosolabs.com>
2023-05-29 09:14:07 +0530

    create christma-lights package

---
+++ src/christmasLights/interfaces/LightGrid.java
@@ -0,0 +1,9 @@
+package christmasLights.interfaces;
+
+public interface LightGrid {
+    void turnOn(int startX, int startY, int endX, int endY);
+    void turnOff(int startX, int startY, int endX, int endY);
+    void toggle(int startX, int startY, int endX, int endY);
+    int getTotalBrightness();
+    int getTotalLightsLit();
+}
